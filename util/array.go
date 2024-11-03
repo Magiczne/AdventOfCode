2ddf85f9366cfa67d29b3073563efb8b6ec4776b
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2024-11-03 16:08:58 +0100

    Day 04

--- util/array.go
+++ util/array.go
@@ -9,3 +9,16 @@ func ArrayMap[T any, U any](sequence []T, mapper func(T) U) []U {
 
 	return data
 }
+
+func Combinations[T any](set []T) (subsets [][]T) {
+	length := len(set)
+	result := [][]T{}
+
+	for i := 0; i < length; i++ {
+		for j := 0; j < length; j++ {
+			result = append(result, []T{set[i], set[j]})
+		}
+	}
+
+	return result
+}
