2b37203edfc94c498a8f789543fb069256d759df
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2024-11-06 17:56:27 +0100

    Day 19 other approach, previous worked only for my input :D

--- d19/main.go
+++ d19/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc2015/util"
+	"regexp"
 	"strings"
 
 	mapset "github.com/deckarep/golang-set/v2"
@@ -12,19 +13,24 @@ type Instruction struct {
 }
 
 type TaskInput struct {
-	Instructions []Instruction
-	Molecule     string
+	Mappings map[string][]string
+	Molecule []string
 }
 
 func part1(data TaskInput) int {
 	seenMolecules := mapset.NewSet[string]()
 
-	for _, instruction := range data.Instructions {
-		for _, foundIndex := range util.IndexAll(data.Molecule, instruction.From) {
-			seenMolecules.Add(
-				data.Molecule[:foundIndex] + instruction.To + data.Molecule[foundIndex+len(instruction.From):],
-			)
+	for moleculePartIndex, moleculePart := range data.Molecule {
+		if mappings, ok := data.Mappings[moleculePart]; ok {
+			for _, mapping := range mappings {
+				data.Molecule[moleculePartIndex] = mapping
+
+				seenMolecules.Add(strings.Join(data.Molecule, ""))
+			}
 		}
+
+		// undo manipulation
+		data.Molecule[moleculePartIndex] = moleculePart
 	}
 
 	return seenMolecules.Cardinality()
@@ -36,18 +42,19 @@ func part2(data TaskInput) int {
 
 func main() {
 	reader := func(name string) TaskInput {
+		regex := regexp.MustCompile(`(?m)([A-Z][a-z]*)`)
 		lines := util.ReadLines(name)
+		mappings := map[string][]string{}
+
+		for _, line := range lines[:len(lines)-2] {
+			splitMapping := strings.Split(line, " => ")
+
+			mappings[splitMapping[0]] = append(mappings[splitMapping[0]], splitMapping[1])
+		}
 
 		return TaskInput{
-			Instructions: util.ArrayMap(lines[:len(lines)-2], func(line string) Instruction {
-				splitInstruction := strings.Split(line, " => ")
-
-				return Instruction{
-					From: splitInstruction[0],
-					To:   splitInstruction[1],
-				}
-			}),
-			Molecule: lines[len(lines)-1],
+			Mappings: mappings,
+			Molecule: regex.FindAllString(lines[len(lines)-1], -1),
 		}
 	}
 
