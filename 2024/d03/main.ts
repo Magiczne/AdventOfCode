23a09b0d3cff4beea6574e3bc8db3d1b886496e6
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2024-12-03 16:30:51 +0100

    cleanup

--- 2024/d03/main.ts
+++ 2024/d03/main.ts
@@ -5,7 +5,7 @@ import { runExamples, runSolution } from '@magiczne/advent-of-code-ts-core/aoc'
 const dontDoMatch = /don't\(\)(.+?)(do\(\)|$)/gs
 const mulMatch = /mul\((\d{1,3}),(\d{1,3})\)/gs
 
-const part1 = (data: string): number => {
+const calculateMulSum = (data: string): number => {
   return [...data.matchAll(mulMatch)]
     .map(match => {
       return [parseInt(match[1], 10), parseInt(match[2], 10)]
@@ -14,15 +14,12 @@ const part1 = (data: string): number => {
     .reduce((acc, result) => acc + result, 0)
 }
 
-const part2 = (data: string): number => {
-  data = data.replaceAll(dontDoMatch, '')
+const part1 = (data: string): number => {
+  return calculateMulSum(data)
+}
 
-  return [...data.matchAll(mulMatch)]
-    .map(match => {
-      return [parseInt(match[1], 10), parseInt(match[2], 10)]
-    })
-    .map(match => match[0] * match[1])
-    .reduce((acc, result) => acc + result, 0)
+const part2 = (data: string): number => {
+  return calculateMulSum(data.replaceAll(dontDoMatch, ''))
 }
 
 const reader = (file: string): string => {
