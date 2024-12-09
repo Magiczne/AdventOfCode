8ee8979dab5a6854d63e549f54ec6b3e3c6e222a
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2024-12-09 22:36:45 +0100

    elixir generator

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,6 +1,58 @@
 import { PlopTypes } from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator('Elixir', {
+    description: 'Generate Typescript AoC day skeleton',
+    prompts: [
+      {
+        type: 'input',
+        name: 'year',
+        message: 'Enter year in which you want to create day skeleton',
+        default: '2021',
+      },
+      {
+        type: 'input',
+        name: 'day',
+        message: 'Enter day which you are working on',
+        default: (new Date()).getDate().toString().padStart(2, '0')
+      },
+      {
+        type: 'list',
+        name: 'withTestRuns',
+        message: 'Do you want to include test runs skeleton?',
+        choices: [
+          { name: 'Yes', value: true },
+          { name: 'No', value: false },
+        ],
+        default: true
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '{{ turbo.paths.root }}/{{ year }}/d{{ day }}/main.exs',
+        templateFile: 'templates/elixir/main.exs.hbs',
+      },
+      {
+        type: 'add',
+        path: '{{ turbo.paths.root }}/{{ year }}/d{{ day }}/input.txt',
+        templateFile: 'templates/elixir/input.txt',
+      },
+      {
+        type: 'add',
+        path: '{{ turbo.paths.root }}/{{ year }}/d{{ day }}/test-runs/0.txt',
+        templateFile: 'templates/elixir/test-runs/0.txt',
+        skip: ({ withTestRuns }: { withTestRuns: boolean }) => {
+          if (withTestRuns) {
+            return false
+          }
+
+          return 'Test runs skipped'
+        }
+      }
+    ]
+  })
+
   plop.setGenerator('Typescript', {
     description: 'Generate Typescript AoC day skeleton',
     prompts: [
